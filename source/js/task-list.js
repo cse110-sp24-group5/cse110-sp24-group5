0ca192a090a9d5e079aa22f9cd8ad3ab7f1368aa
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-18 21:46:41 -0700

    update how tasks are stored in localStorage, and minor bug fixes (clear form after submission, delete redundent event listeners)

--- source/js/task-list.js
+++ source/js/task-list.js
@@ -1,30 +1,62 @@
 window.addEventListener('DOMContentLoaded', init);
 
 /**
- * initializes the page by populating the page with tasks from localStorage 
- * and setting up an event listener for the add task button
+ * initializes the page by setting up event listeners for all the dates, and 
+ * sets up event listeners for moving between months
  */
 function init() {
+    const days = document.querySelectorAll('.days li');
+    //set up event listeners for all the dates so that when a date is clicked, the date's associated task list pops up
+    for(let day of days)
+        day.addEventListener('click', refreshTaskList);
+    //Since days are created dynamically whenever the month changes, this function needs to be re-triggered whenever the month is changed. Otherwise, ...
+    //... the newly created dates won't have event listeners attached to them
+    const prevMonthButton = document.querySelector('.prev-month');
+    const nextMonthButton = document.querySelector('.next-month');
+    prevMonthButton.addEventListener('click', init);
+    nextMonthButton.addEventListener('click', init);
+}
+
+/**
+ * Populates the task list with tasks from the selected date
+ * and sets up an event listener for the add task button
+ */
+function refreshTaskList() {
     addTasksToPage();
     addButton = document.querySelector('#add');
+    //Remove any previous event listeners for the add task button
+    addButton.removeEventListener('click', addTaskButton);
     addButton.addEventListener('click', addTaskButton);
 }
 
 /**
- * Populates page with tasks stored in localStorage
+ * Removes previously created tasks (they may belong to a different date) 
+ * and populates the task list with the selected date's tasks
  */
 function addTasksToPage() {
+    //get previously created tasks
+    const createdTasks = document.querySelectorAll('.task-list-ul li');
+    //loop through previously created tasks and remove them
+    for(let task of createdTasks)
+        task.remove();
+    //get the selected date's tasks
     const taskObjs = getTasksFromStorage();
+    //populate the task list with the selected date's tasks
     for(const taskObj of taskObjs)
         createTask(taskObj);
 }
 
 /**
- * Returns the array of task objects stored in localStorage
- * @returns {Array<Object>} An array of task objects that is stored in localStorage under the key 'tasks'
+ * Returns an array of task objects for the specified date
+ * @returns {Array<Object>} An array of task objects for the specfied date
  */
 function getTasksFromStorage() {
-    return JSON.parse(localStorage.getItem('tasks') || '[]');
+    //get the date selected on the calendar
+    const date = document.getElementById('date').textContent;
+    //get an object containing all tasks from localStorage
+    const allTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    //return an array containing all tasks for a specified day
+    return (allTasks[`${date}`] || []);
 }
 
 
@@ -167,6 +199,8 @@ function closePopUp() {
     }, {once: true});
     const closePopUpButton = document.querySelector('#closePopUp');
     closePopUpButton.removeEventListener('click', () => {closePopUp();}, {once: true});
+    //getFormData also flushes any user input, so use it here to reset the form (This isn't the most elegant way to do this, but it works... Feel free to modify)
+    const trashTaskObj = getFormData()
 }
 
 /**
@@ -202,7 +236,14 @@ function addTaskToStorage(taskObj) {
  * saved to localStorage
  */
 function saveTasksToStorage(taskObjs) {
-    localStorage.setItem('tasks', JSON.stringify(taskObjs));
+    //gets all tasks from localStorage
+    let allTasks = JSON.parse(localStorage.getItem('tasks') || '{}');
+    //gets the date that was selected on the calendar
+    const date = document.getElementById('date').textContent;
+    //modifies the tasks for the selected date in the object containing all tasks
+    allTasks[`${date}`] = taskObjs;
+    //store the object containing all tasks to localStorage under the key 'tasks'
+    localStorage.setItem('tasks', JSON.stringify(allTasks));
 }
 
 /**
@@ -221,9 +262,13 @@ function deleteTaskInStorage(taskObj) {
  * @returns {Object} Returns an object representing a task
  */
 function getFormData() {
-    return {
+    const taskObjPrototype = {
         "id": Math.floor(Math.random() * 1000000),
         "title": document.querySelector('#titleText').value,
         "desc": document.querySelector('#descText').value
     };
+    //flush any user input, so that the form is empty when it's opened again
+    document.querySelector('#titleText').value = "";
+    document.querySelector('#descText').value = "";
+    return taskObjPrototype;
 }
