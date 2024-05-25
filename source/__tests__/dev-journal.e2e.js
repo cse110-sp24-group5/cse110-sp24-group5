89b7a10cb0421e4d0ad05ec68932b41a338ad4a8
Drishti Regmi <dregmi@ucsd.edu>
2024-05-24 20:40:44 -0700

    some ideas for the dev journal end to end testing

---
+++ source/__tests__/dev-journal.e2e.js
@@ -0,0 +1,21 @@
+describe('Dev Journal test suite', () => {
+    //Start by visiting the dev-journal part of our webapp
+    beforeAll(async () => {
+        await page.goto('https://cse110-sp24-group5.github.io/cse110-sp24-group5/source/html/dev-journal');
+    });
+
+    //for the tests here are some of the things that we need to do
+
+    //click on the markdown file, input some text
+
+    //click preview, see if the text appears correctly
+
+    //press save and check if everything is saved correctly
+
+    //click on some of the roles fulfilled
+
+    //check if they are highlighted in the proper color
+
+    //enter text on the bug tracker and learnings and check if that's saved
+
+});
