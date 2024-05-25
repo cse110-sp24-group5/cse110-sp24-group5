5d6fa46b10b4a4f37da4254d45858dfa14a9d4ae
Arnav710 <mailto.arnav.modi@gmail.com>
2024-05-25 12:02:43 -0700

    test for markdown + datepicker

--- source/__tests__/dev-journal.e2e.js
+++ source/__tests__/dev-journal.e2e.js
@@ -25,10 +25,63 @@ describe('Dev Journal Website', () => {
     /**
      * Test if the date on page changes once datepicker is clicked
      */
-    it('', async () => {
+    it('Date change', async () => {
+        const datepicker = await page.$('#datepicker');
+        const newDate = '2024-01-01';
         
-        expect(1).toBe(1);
+        // Evaluate function to set the datepicker value and dispatch change event
+        await page.evaluate((element, date) => {
+            element.value = date;
+            // sending the change event on datepicker
+            element.dispatchEvent(new Event('change'));
+        }, datepicker, newDate);
+
+        
+        const titleHandle = await page.$('#title');
+        const titleText = await titleHandle.evaluate(el => el.textContent);
+
+        expect(titleText).toBe(newDate);
     }, 10000);
 
-   
+    // writing in the markdown editor
+    it('Writing in markdown editor and saving', async () => {
+        
+        // set date
+        const datepicker = await page.$('#datepicker');
+        const newDate = '2024-01-01';        
+        await page.evaluate((element, date) => {
+            element.value = date;
+            element.dispatchEvent(new Event('change'));
+        }, datepicker, newDate);
+
+    
+        // Wait for the editor to be available
+        await page.waitForSelector('#markdown-editor');
+        const editorHandle = await page.$('#markdown-editor');
+    
+        // Click on the editor to focus it
+        await editorHandle.click();
+    
+        // Content to be typed into the editor
+        const markdownContent = '#Hello World\nThis is some sample content';
+        await page.keyboard.type(markdownContent);
+    
+        const buttonHandle = await page.$('.save-button');
+        console.log(buttonHandle);
+        // Click the save button
+        page.on('dialog', async dialog => {
+            await dialog.accept(); // Press the "OK" button on the dialog
+        });
+        await buttonHandle.click();
+
+        // Wait for a short while to ensure the save operation completes
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        
+        const localStorageContent = await page.evaluate(() => {return localStorage.getItem("2024-01-01");})
+
+        // Expect the editor content to be the inserted content
+        expect(JSON.parse(localStorageContent).markdownEditor).toBe(markdownContent); 
+    }, 20000);
+    
+
 });
