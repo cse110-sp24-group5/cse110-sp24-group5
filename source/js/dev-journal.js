fde9d36a3817e272b717bc39eb7d6c024aff1e66
Arnav710 <mailto.arnav.modi@gmail.com>
2024-05-26 11:52:39 -0700

    fix data representation format for dev journal

--- source/js/dev-journal.js
+++ source/js/dev-journal.js
@@ -80,6 +80,17 @@ function showEditor(){
  */
 function saveData() {
     const currentDate = document.getElementById('datepicker').value;
+
+    if (!localStorage) {
+        alert("Local storage is not supported on this browser. Data cannot be loaded.");
+    }
+
+    let jsonData = localStorage.getItem('dev-journal');
+    jsonData = JSON.parse(jsonData);
+    if (!jsonData){
+        jsonData = {};
+    }
+
     const markdownEditor = document.getElementById('markdown-editor').value;
     const roleFulfilled = {
         documentation: document.getElementById('documentationCheckbox').checked,
@@ -98,12 +109,12 @@ function saveData() {
         reflections: reflections
     };
 
-    // Convert the data object to JSON
-    const jsonData = JSON.stringify(data);
+    jsonData[currentDate] = data;
+    console.log(jsonData);
 
     // Save the JSON data to local storage with the current date as the key
     if (localStorage) {
-        localStorage.setItem(currentDate, jsonData);
+        localStorage.setItem('dev-journal', JSON.stringify(jsonData));
         alert("Data saved successfully!");
     } else {
         alert("Local storage is not supported on this browser. Can not save data!");
@@ -120,10 +131,17 @@ function loadData() {
         alert("Local storage is not supported on this browser. Data cannot be loaded.");
     }
 
-    const jsonData = localStorage.getItem(currentDate);
+    const jsonData = localStorage.getItem('dev-journal');
+    let data = JSON.parse(jsonData);
 
-    if (!jsonData) {
+    console.log(data);
+    if (!data){
+        data = {};
+    }
+
+    if (!data.hasOwnProperty(currentDate)) {
         // set to default - empty strings and unselected
+        console.log('Data for day never saved')
         document.getElementById('markdown-editor').value = '';
         document.getElementById('documentationCheckbox').checked = false;
         document.getElementById('codingCheckbox').checked = false;
@@ -133,7 +151,10 @@ function loadData() {
         document.getElementById('learnings').value = '';
         return;
     }
-    const data = JSON.parse(jsonData);
+
+    data = data[currentDate];
+
+    console.log(data);
 
     // Set values from data object to corresponding elements
     document.getElementById('markdown-editor').value = data.markdownEditor;
