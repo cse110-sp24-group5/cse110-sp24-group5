864ac7bd7309beeda9f70f96fb86517c2f2e670f
Angelsofttoy <yiren@ucsd.edu>
2024-05-27 13:25:45 -0700

    image slider working properly, not able to make the slider icon change dynamically yet

--- source/js/sentiment-widget.js
+++ source/js/sentiment-widget.js
@@ -1,4 +1,4 @@
-// changes emoji when user uses slider
+/*// changes emoji when user uses slider
 let currentSentiment = document.querySelector('.sentiment');
 currentSentiment.addEventListener("change", (event) => {
     let current = event.target.value;
@@ -16,4 +16,24 @@ currentSentiment.addEventListener("change", (event) => {
 
     }
 
-});
+});*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sentimentSlider = document.getElementById('sentiment');
+  
+    sentimentSlider.addEventListener('input', () => {
+      const value = sentimentSlider.value;
+      console.log("Value:", value);
+      updateSliderThumb(value);
+    });
+    
+    // Function to update the CSS variable based on slider value
+    function updateSliderThumb(value) {
+      document.documentElement.style.setProperty('--emoji1', value <= 33 ? 'url(\'../img/Group 8.svg\')' : 'none');
+      document.documentElement.style.setProperty('--emoji2', (value > 33 && value <= 66) ? 'url(\'../img/Group 10.svg\')' : 'none');
+      document.documentElement.style.setProperty('--emoji3', value > 66 ? 'url(\'../img/Group 10.svg\')' : 'none');
+    }
+    
+    // Initial update
+    sentimentSlider.dispatchEvent(new Event('input'));
+  });
