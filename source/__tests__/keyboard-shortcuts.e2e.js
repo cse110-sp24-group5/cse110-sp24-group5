8a8801fdb35b8beabeac01e2687021d4d54c7317
Drishti Regmi <dregmi@ucsd.edu>
2024-06-04 20:41:37 -0700

    committing some changes to dev to see if improvement made

---
+++ source/__tests__/keyboard-shortcuts.e2e.js
@@ -0,0 +1,11 @@
+const URL = 'https://cse110-sp24-group5.github.io/cse110-sp24-group5/source/html/index.html';
+
+describe('Test to ensure all keyboard shortcuts are working', () => {
+
+    it('The command \"cd dev-journal\" should take you to the dev-journal')
+
+    })
+
+    
+
+});
