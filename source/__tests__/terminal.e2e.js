29eadbf022ad6a6fa151d9b49c904e291d82145b
stephentong5 <srtong@ucsd.edu>
2024-06-05 20:13:45 -0700

    seperated terminal tests and added outline others

--- source/__tests__/terminal.e2e.js
+++ source/__tests__/terminal.e2e.js
@@ -60,7 +60,7 @@ describe('Terminal test suite', () => {
         expect(terminalClass.includes('hidden')).toBe(true); // Check if the terminal is hidden
       });
 
-    it('should handle terminal input correctly', async () => {
+    it('should cd to calendar', async () => {
         // Simulate pressing Ctrl+/ to toggle the terminal visibility
         await page.keyboard.down('Control');
         await page.keyboard.press('/');
@@ -80,7 +80,107 @@ describe('Terminal test suite', () => {
         const curr_URL = await page.url();
         // checks for expected calandar url
         expect(curr_URL).toBe('https://cse110-sp24-group5.github.io/cse110-sp24-group5/source/html/calendar.html');
+    });
 
+    it('should choose date', async () => {
+
+    });
+
+    it('should choose month and year', async () => {
+
+    });
+
+    it('should delete task', async () => {
+
+    });
+
+    it('should edit task', async () => {
+
+    });
+
+    it('should cd back to home from calendar', async () => {
+        // Simulate pressing Ctrl+/ to toggle the terminal visibility
+        await page.keyboard.down('Control');
+        await page.keyboard.press('/');
+        await page.keyboard.up('Control');
+
+        // Wait for the terminal element to appear
+        await page.waitForSelector('#terminal');
+        
+        // Navigate back to the home page by typing 'cd ..' and pressing Enter
+        await page.click('#terminal-input');
+        await page.keyboard.type('cd ..\n');
+
+        // answers the dialogue asking for name
+        page.once('dialog', async dialog => {
+            console.log(dialog.message());
+            // name of user is rejected now
+            await dialog.dismiss();
+        });
+
+        // Wait for home page to load
+        await page.waitForNavigation();
+        // gets home url
+        const return_URL = await page.url();
+        // checks if returned to home url
+        expect(return_URL).toBe('https://cse110-sp24-group5.github.io/cse110-sp24-group5/source/html/index.html');
+    });
+
+    it('should cd to dev journal', async () => {
+        // Simulate pressing Ctrl+/ to toggle the terminal visibility
+        await page.keyboard.down('Control');
+        await page.keyboard.press('/');
+        await page.keyboard.up('Control');
+
+        // Wait for the terminal element to appear
+        await page.waitForSelector('#terminal');
+        // Click on the terminal input to focus it
+        await page.click('#terminal-input');
+        // Type a command 'cd calendar' and press Enter
+        await page.keyboard.type('cd dev-journal\n');
+
+        // Wait for calendar page to load
+        await page.waitForNavigation();
+
+        // gets the url of the page after load
+        const curr_URL = await page.url();
+        // checks for expected calandar url
+        expect(curr_URL).toBe('https://cse110-sp24-group5.github.io/cse110-sp24-group5/source/html/dev-journal.html');
+    });
+
+    it('should preview the markdown', async () => {
+
+    });
+
+    it('should edit the markdown', async () => {
+
+    });
+
+    it('should write learnings', async () => {
+
+    });
+
+    it('should write bugs', async () => {
+
+    });
+
+    it('should choose debugging role', async () => {
+
+    });
+
+    it('should choose documentation role', async () => {
+
+    });
+
+    it('should choose coding role', async () => {
+
+    });
+
+    it('should choose communication role', async () => {
+
+    });
+
+    it('should cd back to home from dev journal', async () => {
         // Simulate pressing Ctrl+/ to toggle the terminal visibility
         await page.keyboard.down('Control');
         await page.keyboard.press('/');
